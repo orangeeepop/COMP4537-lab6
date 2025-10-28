4b9ada58f9b1ffd7a87518e3aada36bebccf4902
orangeeepop <wonderlandwonders@outlook.com>
2025-10-28 11:28:30 -0700

    Update patientService.js

--- modules/patientService.js
+++ modules/patientService.js
@@ -1,23 +1,28 @@
 const utils = require("./utils");
 
 function createTable(db) {
-  db.query("CREATE DATABASE IF NOT EXISTS labDB;", (err) => {
-    if (err) throw err;
-    db.query("USE labDB;", (err2) => {
-      if (err2) throw err2;
-      db.query(
-        `
-                CREATE TABLE IF NOT EXISTS patient (
-                    patientid INT(11) AUTO_INCREMENT PRIMARY KEY,
-                    name VARCHAR(100),
-                    dateOfBirth DATETIME
-                ) ENGINE=InnoDB;
-            `,
-        (err3) => {
-          if (err3) throw err3;
-          console.log("Database and table are ready.");
-        }
-      );
+  return new Promise((resolve, reject) => {
+    db.query("CREATE DATABASE IF NOT EXISTS labDB;", (err) => {
+      if (err) return reject(err);
+
+      db.query("USE labDB;", (err2) => {
+        if (err2) return reject(err2);
+
+        db.query(
+          `
+          CREATE TABLE IF NOT EXISTS patient (
+            patientid INT(11) AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(100),
+            dateOfBirth DATETIME
+          ) ENGINE=InnoDB;
+          `,
+          (err3) => {
+            if (err3) return reject(err3);
+            console.log("Database and table are ready.");
+            resolve();
+          }
+        );
+      });
     });
   });
 }
